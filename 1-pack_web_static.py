11acfe9623dccebc04d3211ae704ced3d29863f1
Eyiniola <e.fagbemi@alustudent.com>
2024-02-15 10:25:24 -0800

    task

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,19 +1,24 @@
 #!/usr/bin/python3
-""" Function that compress a folder """
+"""
+    script that generates '.tgz' archive from the contents of the 'web_static'
+"""
+from fabric.api import local
 from datetime import datetime
-from fabric import task
-import os
 
 
 def do_pack():
-    """The function that compress a folder before sending"""
-    try:
-        if not os.path.exists("versions"):
-            local('mkdir versions')
-        t = datetime.now()
-        f = "%Y%m%d%H%M%S"
-        archive_path = 'versions/web_static_{}.tgz'.format(t.strftime(f))
-        local('tar -cvzf {} web_static'.format(archive_path))
+    """
+        function to compress directory into .tgz archive
+        Return: Success - '.tgz' archive path
+                Failure - None
+    """
+    now = datetime.now()
+    now = now.strftime('%Y%m%d%H%M%S')
+    archive_path = 'versions/web_static_' + now + '.tgz'
+
+    local('mkdir -p versions/')
+    result = local('tar -cvzf {} web_static/'.format(archive_path))
+
+    if result.succeeded:
         return archive_path
-    except:
-        return None
+    return None
