af8a1580f2288478128895f357f92cc97f53398d
Eyiniola <e.fagbemi@alustudent.com>
2024-02-22 03:29:05 -0800

    task 1

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+a script that starts a Flask application
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/', '/hbnb', strict_slashes=False)
+def hello_hbnb():
+    return 'Hello HBNB'
+def hbnb():
+    return "HBNB"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
