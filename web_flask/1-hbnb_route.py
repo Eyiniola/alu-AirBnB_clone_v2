03349ab8da9a9b4028e69aafa27042d08c4983d1
Eyiniola <e.fagbemi@alustudent.com>
2024-02-24 11:37:36 -0800

    task 1

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -9,19 +9,23 @@ It defines two routes:
 Requirements:
 - Your web application must be listening on 0.0.0.0, port 5000
 """
+
+
 from flask import Flask
 
 app = Flask(__name__)
 
 
-@app.route('/', '/hbnb', strict_slashes=False)
-def hello_hbnb():
+@app.route("/", strict_slashes=False)
+def index():
+    """Route for the root URL ("/")."""
     return "Hello HBNB!"
 
 
-@app.route('/hbnb', strict_slashes=False)
-def hbnb():
+@app.route("/hbnb", strict_slashes=False)
+def about():
+    """Route for "/hbnb"."""
     return "HBNB"
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
